610eb625f4c5cf16be443535c78bc5c4c8f1376d
utkarshvivek <utkarshvivekeps9@gmail.com>
2025-06-02 23:39:43 +0530

    changes made

--- src/main/java/javaapp/jenkins.java
+++ src/main/java/javaapp/jenkins.java
@@ -6,6 +6,7 @@ public class jenkins {
 		// TODO Auto-generated method stub
         System.out.println("hello");
         System.out.println("hellooooooo");
+        System.out.println("i am doing jenkins");
 	}
 
 }
